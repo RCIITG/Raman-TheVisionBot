09ba17ee2c6a5617117c33b44bafb105340d9215
Rohit3475 <rohit170103057@iitg.ac.in>
2020-03-20 16:57:26 +0530

    ROS package22

---
+++ object_detection/src1/call.py
@@ -0,0 +1,4 @@
+import json
+d2 = json.load(open('Text.txt'))
+print d2["color"]
+
---
+++ object_detection/src1/object_detection1.py
@@ -0,0 +1,74 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+import cv2
+from PIL import Image
+from matplotlib import pyplot as plt
+sys.path.append('..')
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile('./ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb', 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+label_map = label_map_util.load_labelmap('./data/mscoco_label_map.pbtxt')
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=90, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+#matplotlib nbagg
+cap = cv2.VideoCapture(0)
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    while True:
+      image_valid ,image_np = cap.read()
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      #image_np = load_image_into_numpy_array(image)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      # Actual detection.
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      cv2.imshow('image', image_np)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
